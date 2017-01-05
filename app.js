73e4df10e0e5b56297d6ab38d5128e1f5fcdd54c
Dr. Mike Hopper <drmikeh@gmail.com>
2017-01-04 20:20:29 -0500

    Added the INDEX and SHOW client routes.

--- app.js
+++ app.js
@@ -22,10 +22,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/movies', movies);
+app.use('/api/movies', movies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/javascripts/client.js
@@ -0,0 +1,52 @@
+angular.module('moviesApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: "/",
+      template: "<h3>The Home Route</h3>"
+    })
+		.state('movies', {
+			url: "/movies",
+			templateUrl: "/templates/movies.html",
+      controller: "moviesController",
+      controllerAs: "$ctrl"
+		})
+    .state('movies-show', {
+      url: "/movies/:id",
+      templateUrl: "/templates/movies-show.html",
+      controller: "moviesShowController",
+      controllerAs: "$ctrl"
+    });
+
+  $urlRouterProvider.otherwise("/");
+})
+.controller('moviesController', function($http) {
+  this.movies = [];
+  $http.get('/api/movies')
+  .then( (response) => {
+    this.movies = response.data.movies;
+  })
+  .catch(function(err) {
+    alert(err);
+  });
+})
+.controller('moviesShowController', function($http, $stateParams) {
+  this.movie = {};
+  const id = $stateParams.id;
+  console.log('getting movie with id=', id);
+  $http.get('/api/movies/' + id)
+  .then( (response) => {
+    this.movie = response.data.movie;
+  })
+  .catch(function(err) {
+    alert(err);
+  });
+})
+
+
+// .component('movies', {
+//   template: '<h1>Here are some Movies</h1>',
+//   controller: function() {
+//     console.log('movies controller is alive!');
+//   }
+// });
